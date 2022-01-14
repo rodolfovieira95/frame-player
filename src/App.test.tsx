323a7658668aede75d108cbb041428eaadd9e020
Rodolfo Vieira <rodolfovieira95@outlook.com>
2022-01-14 12:48:06 -0300

    housekeeping: update app.tsx test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders all the FramePlayer components", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const exhibitionArea = screen.getByTestId(/exhibition-area/i);
+  expect(exhibitionArea).toBeInTheDocument();
+
+  const progressBar = screen.getByTestId(/progress-bar/i);
+  expect(progressBar).toBeInTheDocument();
+
+  const controls = screen.getByTestId(/controls/i);
+  expect(controls).toBeInTheDocument();
 });
