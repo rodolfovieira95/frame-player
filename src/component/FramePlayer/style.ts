02d4bf256f2004dfef7c5890f948b76aecef9a0b
Rodolfo Vieira <rodolfovieira95@outlook.com>
2022-01-14 03:19:04 -0300

    housekeeping: fix some dimensions and add some new styles

--- src/component/FramePlayer/style.ts
+++ src/component/FramePlayer/style.ts
@@ -2,16 +2,16 @@ import styled from "styled-components";
 import { SlideProps, SliderContentProps } from "./types";
 
 export const SliderContent = styled.div<SliderContentProps>`
-  transform: translateX(-${(props) => 1280 * (2 * props.currentFrame)}px);
-  transition: transform ease-out ${(props) => props.transition}s;
+  transform: translateX(-${(props) => 1280 * props.currentFrame}px);
+  transition: transform ease-out 0s;
   height: 100%;
   width: ${(props) => props.width}px;
   display: flex;
 `;
 
 export const Slide = styled.div<SlideProps>`
-  height: 100%;
-  width: 100%;
+  height: auto;
+  width: 1280px;
   background-image: url(${(props) => props.frame});
   background-size: cover;
   background-repeat: no-repeat;
@@ -24,4 +24,16 @@ export const Container = styled.div`
   height: 720px;
   margin: 0 auto;
   overflow: hidden;
+  background-color: black;
+`;
+
+export const ControlContainer = styled.div`
+  margin: 20px auto;
+  width: 1280px;
+  display: flex;
+  gap: 20px;
+`;
+
+export const Button = styled.div`
+  cursor: pointer;
 `;
