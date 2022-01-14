e105b03d498a994e6d1ed50c18e9959042253b90
Rodolfo Vieira <rodolfovieira95@outlook.com>
2022-01-14 03:20:15 -0300

    housekeeping: refactor frame player

--- src/component/FramePlayer/index.tsx
+++ src/component/FramePlayer/index.tsx
@@ -1,56 +1,55 @@
-import { useEffect, useState } from "react";
-import { ProgressBar } from "../Slider";
+import { useEffect, useRef, useState } from "react";
 
-import { Container, Slide, SliderContent } from "./style";
+import {
+  Button,
+  Container,
+  ControlContainer,
+  Slide,
+  SliderContent,
+} from "./style";
 import { Props } from "./types";
+import { counterToTime, getCurrentFrame } from "./interface";
+
+import { ProgressBar } from "../Slider";
 import Play from "../../assets/play.svg";
 import Pause from "../../assets/pause.svg";
 
 export const FramePlayer = ({ frames, fps }: Props) => {
   const [state, setState] = useState({
-    translate: 0,
-    transition: 0.45,
     isPlaying: false,
     counter: 0,
   });
-  const { translate, transition, isPlaying, counter } = state;
+  const { isPlaying, counter } = state;
 
+  const timerRef = useRef<number>();
   const finishCounter = counter < (1 / fps) * frames.length;
-  const actualBarPosition = (1280 * counter) / ((1 / fps) * frames.length);
-
-  const getCurrentFrame = () => {
-    const timeToChangeFrame = 1 / fps;
-    const quociente = Math.trunc(counter / timeToChangeFrame);
-    const rest = counter % timeToChangeFrame;
-
-    return quociente;
-  };
 
   useEffect(() => {
-    isPlaying &&
-      finishCounter &&
-      setTimeout(
-        () =>
-          setState((currState) => ({
-            ...currState,
-            counter: currState.counter + 1,
-          })),
-        1000
-      );
-  }, [counter, isPlaying, finishCounter]);
+    if (isPlaying && finishCounter) {
+      timerRef.current = setInterval(() => {
+        setState((currState) => ({
+          ...currState,
+          counter: counter + 1,
+        }));
+      }, 1000) as unknown as number;
+    } else if (!isPlaying && counter !== 0) {
+      clearInterval(timerRef.current);
+    }
+    return () => clearInterval(timerRef.current);
+  });
 
   const getWidth = () => window.innerWidth;
 
   const playFunction = () => {
     setState((currState) => ({
       ...currState,
-      isPlaying: !currState.isPlaying,
+      isPlaying: !isPlaying,
     }));
   };
 
-  const PlayPauseButton = () => {
+  const PlayPauseIcon = () => {
     return isPlaying ? (
-      <img src={Pause} alt="play button" width={16} />
+      <img src={Pause} alt="pause button" width={16} />
     ) : (
       <img src={Play} alt="play button" width={16} />
     );
@@ -60,30 +59,25 @@ export const FramePlayer = ({ frames, fps }: Props) => {
     <>
       <Container>
         <SliderContent
-          customTranslate={translate}
-          transition={transition}
           width={getWidth() * frames.length}
-          currentFrame={getCurrentFrame()}
+          currentFrame={getCurrentFrame(fps, counter)}
         >
           {frames.map((frame, i) => (
             <Slide key={i} frame={frame} />
           ))}
         </SliderContent>
       </Container>
-      <ProgressBar actualPosition={actualBarPosition} />
-      <div
-        style={{
-          margin: "20px auto",
-          width: "1280px",
-          display: "flex",
-          gap: "20px",
-        }}
-      >
-        <div onClick={() => playFunction()} style={{ cursor: "pointer" }}>
-          <PlayPauseButton />
-        </div>
-        <div>{counter}</div>
-      </div>
+      <ProgressBar
+        counter={counter}
+        setState={setState}
+        finishCounter={(1 / fps) * frames.length}
+      />
+      <ControlContainer>
+        <Button onClick={playFunction}>
+          <PlayPauseIcon />
+        </Button>
+        <div>{counterToTime(counter)}</div>
+      </ControlContainer>
     </>
   );
 };
